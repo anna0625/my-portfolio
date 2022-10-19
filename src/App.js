4106d5ba333704a64f01a625f953b9751bec81e1
anna0625 <anna860625@gmail.com>
2022-10-19 18:01:31 +1100

    add React in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import ReactProjects from "./pages/ReactProjects";
 import MiniProjects from "./pages/MiniProjects";
